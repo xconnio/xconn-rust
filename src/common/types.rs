f0d89c521b7cba6945e4e619eb39b639841b8251
Ismail Akram <ismail@xconn.io>
2026-01-13 19:11:20 +0500

    Do not prepend Error: Error:

--- src/common/types.rs
+++ src/common/types.rs
@@ -24,7 +24,7 @@ impl Error {
 // Implement Display so errors print nicely
 impl fmt::Display for Error {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(f, "Error: {}", self.message)
+        write!(f, "{}", self.message)
     }
 }
 
